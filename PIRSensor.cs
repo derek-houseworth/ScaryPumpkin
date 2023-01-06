1ef52df181aa6ae6b7f15ce6b29802ae67a6777b
Derek Houseworth <derek@houseworthconsulting.com>
2023-01-06 12:58:37 -0700

    Add project files.

---
+++ PIRSensor.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScaryPumpkin
+{
+    public class PIRSensor
+    {
+
+        //Parallax PIR proximity/motion sensor Rev. A
+        //https://www.parallax.com/product/555-28027
+
+        public string Name;
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        //true after successful initialization of sensor
+        public bool Initialized { get; internal set; }
+
+        //current status of sensor: true if motion is being detected, false otherwise
+        public bool DetectingMotion { get; private set; } = false;
+
+        public delegate void MotionDectedHandler();
+        public event MotionDectedHandler MotionDetected;
+
+        //number of Raspberry Pi GPIO pin for sensor input
+        public int Pin { get; }
+        private GpioController _controller;
+
+        /// <summary>
+        /// calling application code sets IsRunning to true to start listening for signal from sensor, false to stop
+        /// </summary>
+        private bool _isRunning = false;
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set
+            {
+                if (_isRunning != value)
+                {
+                    if (MotionDetected is null)
+                    {
+                        throw new ArgumentException($"{Name}'s MotionDetected event handler has not been set");
+                    }
+                    _isRunning = value;
+                    if (IsRunning)
+                    {
+                        Start();
+                    }
+                    else if (_cancellationTokenSource is not null)
+                    {
+                        if (!_cancellationTokenSource.IsCancellationRequested)
+                        {
+                            Console.WriteLine($"{Name}: stopping");
+                            _cancellationTokenSource?.Cancel();
+                        }
+                    }
+
+                }
+            }
+
+        } //IsRunning
+
+
+        /// <summary>
+        /// attempts to instantiate GPIO controller object and set mode of sensor pin
+        /// </summary>
+        private void InitializeController()
+        {
+            Initialized = false;
+            try
+            {
+                _controller = new GpioController();
+                if (_controller is not null)
+                {
+                    _controller.OpenPin(Pin, PinMode.Output);
+                    _controller.Write(Pin, PinValue.Low);
+                    _controller.SetPinMode(Pin, PinMode.Input);
+                    Initialized = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{Name}: {ex.Message}");
+            }
+
+            Console.WriteLine($"{Name}: initialize sensor on pin {Pin:N0} {(Initialized ? "SUCCESS" : "ERROR")}");
+        }
+
+
+        /// <summary>
+        /// initialize GPIO controller if necessary and start task to listen for sensor output
+        /// </summary>
+        private async void Start()
+        {
+            if (_controller is null)
+            {
+                InitializeController();
+            }
+            try
+            {
+                using (_cancellationTokenSource = new())
+                {
+                    await ReadSensor(_cancellationTokenSource.Token);
+                }
+            }
+            catch (TaskCanceledException tce)
+            {
+                Console.WriteLine($"{Name}: {tce.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{Name}: {ex.Message}");
+            }
+            finally
+            {
+                _cancellationTokenSource = null;
+            }
+
+        } //Start
+
+
+        /// <summary>
+        /// free allocated program resources, called once before app exit
+        /// </summary>
+        public void Shutdown()
+        {
+
+            IsRunning = false;
+            Initialized = false;
+            Console.WriteLine($"{Name}: shutting down");
+            _controller?.Dispose();
+
+        } //Shutdown
+
+
+        /// <summary>
+        /// cancelable task to monitor sensor input pin for sensor to signal motion by outputting high signal 
+        /// MotionDetected handler called when pin value indicates motion detected
+        /// </summary>
+        /// <param name="cancelToken"></param>
+        /// <returns></returns>
+        private Task ReadSensor(CancellationToken cancelToken)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"{Name}: now listening...");
+                while (true)
+                {
+                    bool currentReading = (_controller.Read(Pin) == PinValue.High);
+                    if (currentReading != DetectingMotion)
+                    {
+                        DetectingMotion = currentReading;
+                        if (DetectingMotion)
+                        {
+                            MotionDetected?.Invoke();
+                        }
+                    }
+                    Thread.Sleep(150);
+
+                    cancelToken.ThrowIfCancellationRequested();
+                }
+            }, cancelToken);
+
+        } //ReadSensor
+
+
+        /// <summary>
+        /// sensor object constructor 
+        /// </summary>
+        /// <param name="pin">Raspberry PI GPIO pin to read input connected to sensor output</param>
+        /// <param name="name">string representing name of sensor</param>
+        public PIRSensor(int pin = 18, string name = null)
+        {
+            Pin = pin;
+            IsRunning = false;
+            Name = name ?? "PIR Sensor";
+
+
+        } //PIRSensor
+
+    } //PIRSensor
+
+} //BotUtil
---
+++ ScaryPumpkin.cs
@@ -0,0 +1,219 @@
+﻿using NetCoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Device.Gpio;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScaryPumpkin
+{
+    class ScaryPumpkin
+    {
+
+#if DEBUG
+        private const string BUILD_TYPE = "DEBUG";
+#else
+        private const string BUILD_TYPE = "Release";
+#endif
+
+        //spooky sound effects play list
+        private const string SOUND_EFFECTS_PATH = "./SoundEffects/";
+        //private readonly static ObservableCollection<string> _soundEffectFileNames = new();
+        private readonly static List<string> _soundEffectFileNames = new();
+
+        private static Player _playerSpookySounds;
+
+        private readonly static Random _random = new();
+
+        //LEDs
+        const int LIGHT_SWITCH_PIN = 26;
+
+        //PIR motion sensor
+        const int MOTION_SENSOR_PIN = 18;
+        private static PIRSensor _pirSensor;
+
+        //GPIO controller for light switch 
+        private static GpioController _controller;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ScaryPumpkin: " + GetAppVersion(typeof(ScaryPumpkin).GetTypeInfo().Assembly));
+            Console.WriteLine("ScaryPumpkin: Press Ctrl+C to exit");
+
+            InitializeSoundEffectsList();
+
+            InitializeLightSwitch();
+
+            bool lightsOn = false;
+            
+            //initiaize audo player
+            _playerSpookySounds = new Player();
+            _playerSpookySounds.PlaybackFinished += OnPlaybackFinished;
+
+            //handle Ctrl+C keypress event to cleanup app resouces and exit
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
+            {
+                Shutdown();
+                Environment.Exit(0);
+            };
+
+            //initialize motion detector object
+            _pirSensor = new PIRSensor(MOTION_SENSOR_PIN);
+            _pirSensor.MotionDetected += OnMotionDetected;
+            _pirSensor.IsRunning = true;
+
+            //one-time playback of 'welcome' sound effect concludes app initialization
+            PlaySoundEffectAsync("./SoundEffects/welcome-01.mp3");
+            Thread.Sleep(3000);
+
+            //main loop 
+            //and while sound is playing 
+            while (true)
+            {
+                //blink lights at random intervals after PIR sensor has detected motion and 
+                //while sound effect playback in progress 
+                lightsOn = _playerSpookySounds.Playing ? !lightsOn : false;
+
+                _controller.Write(LIGHT_SWITCH_PIN, lightsOn ? PinValue.High : PinValue.Low);
+                int delayMs = 50;
+                if (_playerSpookySounds.Playing && _random.Next(1, 10) > 5)
+                {
+                    delayMs += _random.Next(20, 140);
+                }
+                Thread.Sleep(delayMs);
+
+            } //main loop
+
+        } //Main
+
+        private static bool InitializeLightSwitch()
+        {
+            bool initialized = false;
+
+            try
+            {
+                _controller = new GpioController();
+                _controller.OpenPin(LIGHT_SWITCH_PIN, PinMode.Output);
+                initialized = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ScaryPumpkin: light switch " + ex.Message);
+            }
+            Console.WriteLine($"ScaryPumpkin: initiaize light switch on pin {LIGHT_SWITCH_PIN:N0} " + (initialized ? "SUCCESS" : "ERROR") + 
+                " on pin " + LIGHT_SWITCH_PIN.ToString());
+
+            return initialized;
+
+        } //InitializeLightSwitch
+
+        /// <summary>
+        /// cleans up allocated resources at application exit
+        /// </summary>
+        private static void Shutdown()
+        {
+            Console.WriteLine("");
+            _pirSensor?.Shutdown();
+            _controller?.Dispose();
+            Console.WriteLine("ScaryPumpkin: shutting down");
+
+        } //Shutdown
+
+
+        /// <summary>
+        /// hanlder for PIRSensor object's MotionDetected event: begins playback of 
+        /// random sound effect if none currently playing 
+        /// </summary>
+        public static void OnMotionDetected()
+        {
+
+            if (!_playerSpookySounds.Playing)
+            {
+                if (0 == _soundEffectFileNames.Count)
+                {
+                    InitializeSoundEffectsList();
+                }
+                string currentSoundEffectFileName = _soundEffectFileNames[_random.Next(0, _soundEffectFileNames.Count - 1)];
+
+                //remove selected sound from list: each sounds in list played once before list is reloaded
+                _soundEffectFileNames.Remove(currentSoundEffectFileName);
+
+                PlaySoundEffectAsync(currentSoundEffectFileName);
+            }
+
+        } //OnMotionDetected
+
+        /// <summary>
+        /// selects and plays random sound from sound effects list property
+        /// </summary>
+        private static async void PlaySoundEffectAsync(string soundEffectFileName)
+        {
+
+            await Task.Run(() =>
+            {
+
+                Console.Write($"ScaryPumpkin: playing {soundEffectFileName[(soundEffectFileName.LastIndexOf('/') + 1)..]}... ");
+                _playerSpookySounds.Play(soundEffectFileName);
+
+            });
+
+        } //PlayRandomSoundAsync
+
+        /// <summary>
+        /// stores names of all .MP3 files from sound effects directory in _soundEffectFileNames property
+        /// </summary>
+        private static void InitializeSoundEffectsList()
+        {
+            try
+            {
+                foreach (string fileName in Directory.EnumerateFiles(SOUND_EFFECTS_PATH, "*.mp3", SearchOption.AllDirectories))
+                {
+                    _soundEffectFileNames.Add(fileName);
+                    //Console.WriteLine(fileName);
+                }
+
+                Console.WriteLine($"ScaryPumpkin: found {_soundEffectFileNames.Count} sound effect files in {SOUND_EFFECTS_PATH}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        } //InitializeSoundEffectsList
+
+
+        /// <summary>
+        /// handler for Player's PlaybackFinished event 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnPlaybackFinished(object sender, EventArgs e)
+        {
+
+            Console.WriteLine("done!");
+
+        } //OnPlaybackFinished
+
+
+        /// <summary>
+        /// gets application name and version information
+        /// </summary>
+        /// <param name="appAsm"></param>
+        /// <returns>string containing application name and version of specified assembly</returns>
+        public static string GetAppVersion(Assembly appAsm)
+        {
+            string appName = appAsm.FullName[..appAsm.FullName.IndexOf(',')];
+            int start = appAsm.FullName.IndexOf('=') + 1;
+            int end = appAsm.FullName.IndexOf(',', start);
+            string version = appAsm.FullName[start..end];
+            return $"{appName} {version}";
+
+        } //GetAppVersion
+
+    } //ScaryPumpkin
+
+}  //ScaryPumpkin
