a67117648af4100b91a8e1d3edc9b65db6b32e3a
Derek Houseworth <derek@houseworthconsulting.com>
2024-11-19 12:07:16 -0700

    add support for loading configuration values from JSON file, reorder start up code, update to .NET 9

--- ScaryPumpkin.cs
+++ ScaryPumpkin.cs
@@ -1,4 +1,5 @@
-﻿using NetCoreAudio;
+﻿using Microsoft.Extensions.Configuration;
+using NetCoreAudio;
 using System;
 using System.Collections.Generic;
 using System.Device.Gpio;
@@ -18,48 +19,75 @@ class ScaryPumpkin
     private const string BUILD_TYPE = "Release";
 #endif
 
-    //spooky sound effects play list
-    private const string SOUND_EFFECTS_PATH = "./SoundEffects/";
-    private readonly static List<string> _soundEffectFileNames = [];
+    //name of JSON file containing app settings key/value pairs, located in project root
+    private const string APP_SETTINGS_FILE = "appsettings.json";
 
+    //keys to retrieve values loaded from appsettings.json config file
+    private const string LIGHT_SWITCH_PIN_CONFIG_KEY = "lightSwitchPin";
+    private const string MOTION_SENSOR_PIN_CONFIG_KEY = "motionSensorPin";
+    private const string SOUND_EFFECTS_PATH_CONFIG_KEY = "soundEffectsPath";
+    private const string WELCOME_SOUND_CONFIG_KEY = "welcomeSound";
+
+    //spooky sound effects play list
+    private readonly static List<string> _soundEffectFileNames = [];
+    private static string _soundEffectsPath = "";
+    private static string _welcomeSound = "";
+
+    //audio player
     private static Player _playerSpookySounds;
 
     private readonly static Random _random = new();
 
-    private static string _appName;
+    private static string _appName = "";
 
     private static readonly DateTime _startTime = DateTime.Now;
 
-	//LEDs
-	const int LIGHT_SWITCH_PIN = 26;
-
     //PIR motion sensor
-    const int MOTION_SENSOR_PIN = 18;
+    private static int _motionSensorPin = 0;
     private static PIRSensor _pirSensor;
 
-    //GPIO controller for light switch 
-    private static GpioController _controller;
+    //transistor to toggle light switch on/off
+    private static int _lightSwitchPin = 0;
+	private static GpioController _controller;
 
     static void Main()
     {
 		_appName = GetAppName(typeof(ScaryPumpkin).GetTypeInfo().Assembly);
-
 		DebugOutput("launching " + GetAppVersion(typeof(ScaryPumpkin).GetTypeInfo().Assembly));
-        DebugOutput("Press Ctrl+C to exit");
+		DebugOutput("press Ctrl+C to exit");
 
-        InitializeSoundEffectsList();
-
-        if (!InitializeLightSwitch())
+        //load settings from JSON file
+		DebugOutput($"loading settings from {APP_SETTINGS_FILE} file...");
+		if (!LoadSettingsFromJson(APP_SETTINGS_FILE))
         {
-			DebugOutput("error initializing GPIO controller, shutting down");
+			DebugOutput($"error reading settings from {APP_SETTINGS_FILE}, shutting down");
 			Environment.Exit(0);
 		}
 
-		bool lightsOn = false;
-        
+		//initialize motion detector object
+		_pirSensor = new PIRSensor(_motionSensorPin);
+		_pirSensor.MotionDetected += OnMotionDetected;
+		
+        //enumerate files in sound effects dir and load sound effects list
+        InitializeSoundEffectsList();
+        if (_soundEffectFileNames.Count == 0)
+        {
+            DebugOutput($"no sound effect files found in {_soundEffectsPath}, shutting down");
+            Environment.Exit(0);
+        }
+
+        //initialize light switch GPIO pin
+        if (!InitializeLightSwitch())
+        {
+            DebugOutput("error initializing GPIO controller, shutting down");
+            Environment.Exit(0);
+        }
+
+        bool lightsOn = false;
+
         //initialize audio player
         _playerSpookySounds = new Player();
-        _playerSpookySounds.PlaybackFinished += OnPlaybackFinished;
+		_playerSpookySounds.PlaybackFinished += OnPlaybackFinished;
 
         //register handler function to respond to  Ctrl+C keypress event by freeing 
         //app resources and exiting
@@ -69,12 +97,8 @@ class ScaryPumpkin
             Environment.Exit(0);
         };
 
-        //initialize motion detector object
-        _pirSensor = new PIRSensor(MOTION_SENSOR_PIN);
-        _pirSensor.MotionDetected += OnMotionDetected;
-
         //one-time playback of 'welcome' sound effect concludes app initialization
-        PlaySoundEffectAsync("./SoundEffects/welcome-01.mp3");
+        PlaySoundEffectAsync(_soundEffectsPath + _welcomeSound);
 
         //main loop 
         while (true)
@@ -83,7 +107,7 @@ class ScaryPumpkin
             //while sound effect playback in progress 
             lightsOn = _playerSpookySounds.Playing ? !lightsOn : false;
 
-            _controller.Write(LIGHT_SWITCH_PIN, lightsOn ? PinValue.High : PinValue.Low);
+            _controller.Write(_lightSwitchPin, lightsOn ? PinValue.High : PinValue.Low);
             int delayMs = 50;
             if (_playerSpookySounds.Playing && _random.Next(1, 10) > 5)
             {
@@ -102,14 +126,14 @@ class ScaryPumpkin
         try
         {
             _controller = new GpioController();
-            _controller.OpenPin(LIGHT_SWITCH_PIN, PinMode.Output);
+            _controller.OpenPin(_lightSwitchPin, PinMode.Output);
             initialized = true;
         }
         catch (Exception ex)
         {
             DebugOutput("light switch " + ex.Message);
         }
-        DebugOutput($"initialize light switch on pin {LIGHT_SWITCH_PIN:N0} " + 
+        DebugOutput($"initialize light switch on pin {_lightSwitchPin:N0} " +
             (initialized ? "SUCCESS" : "ERROR"));
         return initialized;
 
@@ -162,12 +186,14 @@ class ScaryPumpkin
         await Task.Run(() =>
         {
 
-			DebugOutput($"playing {soundEffectFileName[(soundEffectFileName.LastIndexOf('/') + 1)..]}... ");
-            _playerSpookySounds.Play(soundEffectFileName);
+            DebugOutput($"playing {soundEffectFileName[(soundEffectFileName.LastIndexOf('/') + 1)..]}... ");
+            
+			_playerSpookySounds.Play(soundEffectFileName);
 
         });
 
     } //PlayRandomSoundAsync
+
 
     /// <summary>
     /// stores names of all .MP3 files from sound effects directory in _soundEffectFileNames property
@@ -176,17 +202,17 @@ class ScaryPumpkin
     {
         try
         {
-            foreach (string fileName in Directory.EnumerateFiles(SOUND_EFFECTS_PATH, "*.mp3", SearchOption.AllDirectories))
+            foreach (string fileName in Directory.EnumerateFiles(_soundEffectsPath, "*.mp3", SearchOption.AllDirectories))
             {
                 _soundEffectFileNames.Add(fileName);
             }
 
-            DebugOutput($"found {_soundEffectFileNames.Count} sound effect files in {SOUND_EFFECTS_PATH}");
+            DebugOutput($"found {_soundEffectFileNames.Count} sound effect files in {_soundEffectsPath}");
 
         }
         catch (Exception e)
         {
-			DebugOutput(e.Message);
+            DebugOutput(e.Message);
         }
 
     } //InitializeSoundEffectsList
@@ -199,7 +225,7 @@ class ScaryPumpkin
     /// <param name="e"></param>
     private static void OnPlaybackFinished(object sender, EventArgs e)
     {
-		DebugOutput("done!");
+        DebugOutput("done!");
 
         if (!_pirSensor.IsRunning)
         {
@@ -210,15 +236,15 @@ class ScaryPumpkin
     } //OnPlaybackFinished
 
 
-	/// <summary>
-	/// gets application name, version & build type retrieved from 
+    /// <summary>
+    /// gets application name, version & build type retrieved from 
     /// assembly specified by appAsm parameter
-	/// </summary>
-	/// <param name="appAsm"></param>
-	/// <returns>string containing application name, version and build type for specified assembly</returns>
-	private static string GetAppVersion(Assembly appAsm)
+    /// </summary>
+    /// <param name="appAsm"></param>
+    /// <returns>string containing application name, version and build type for specified assembly</returns>
+    private static string GetAppVersion(Assembly appAsm)
     {
-    	int start = appAsm.FullName.IndexOf('=') + 1;
+        int start = appAsm.FullName.IndexOf('=') + 1;
         int end = appAsm.FullName.IndexOf(',', start);
         string version = appAsm.FullName[start..end];
         return $"{version} {BUILD_TYPE}";
@@ -233,23 +259,59 @@ class ScaryPumpkin
     /// <param name="appAsm"></param>
     /// <returns></returns>
     public static string GetAppName(Assembly appAsm)
-    { 
+    {
         return appAsm.FullName[..appAsm.FullName.IndexOf(',')];
 
-	} //GetAppName
+    } //GetAppName
 
 
-	/// <summary>
-	/// writes current system time, app name and message string parameter value to console
-	/// </summary>
-	/// <param name="message"></param>
-	private static void DebugOutput(string message)
-	{
+    /// <summary>
+    /// writes current system time, app name and message string parameter value to console
+    /// </summary>
+    /// <param name="message"></param>
+    private static void DebugOutput(string message)
+    {
 
         TimeSpan elapsed = DateTime.Now - _startTime;
-		Console.WriteLine($"{elapsed.ToString(@"hh\:mm\:ss")} {_appName}: {message}");
+        Console.WriteLine($"{elapsed:hh\\:mm\\:ss} {_appName}: {message}");
 
 	} //DebugOutput
 
+
+    // loads app settings values from JSON format file specified in jsonSettingsFileName parameter
+    // example file format: 
+    //  {
+    //      "motionSensorPin": 18,
+    //      "lightSwitchPin": 26,
+    //      "soundEffectsPath": "./SoundEffects/",
+    //      "welcomeSound": "welcome-01.mp3"
+    //  }
+    private static bool LoadSettingsFromJson(string jsonSettingsFileName)
+    {
+        if (!File.Exists(jsonSettingsFileName))
+        {
+            return false;
+        }
+
+        try
+        {
+            // load config settings from appsettings.json file:
+            IConfigurationRoot configRoot = new ConfigurationBuilder()
+            .AddJsonFile(jsonSettingsFileName, false, true)
+            .Build();
+		
+            _motionSensorPin = Convert.ToInt16(configRoot[MOTION_SENSOR_PIN_CONFIG_KEY]);
+			_lightSwitchPin = Convert.ToInt16(configRoot[LIGHT_SWITCH_PIN_CONFIG_KEY]);
+			_soundEffectsPath = configRoot[SOUND_EFFECTS_PATH_CONFIG_KEY];
+			_welcomeSound = configRoot[WELCOME_SOUND_CONFIG_KEY];
+		}
+		catch (Exception)
+		{
+            return false;
+        }
+
+		return true;
+
+	} //LoadSettingsFromJson
 
 } //ScaryPumpkin
